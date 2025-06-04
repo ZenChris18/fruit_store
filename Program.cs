9fa161993252e682b6b438f049184ab9e317cb6e
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-06-04 22:55:32 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What Fruit do you want to buy?: ");
+        string? fruit = Console.ReadLine();
+        if (fruit != null)
+        {
+            Price(fruit);
+        }
+        else
+        {
+            Console.WriteLine("No fruit entered");
+        }
+    }
+
+    // Greeting method
+    static void Price(string name)
+    {
+        char symbol = '$';
+        if (name == "Apple")
+        {
+            Console.WriteLine("The price of apple is 70" + symbol);
+        }
+        else if (name == "Mango")
+        {
+            Console.WriteLine("The price of mango is 50" + symbol);
+        }
+        else
+        {
+            Console.WriteLine("Fruit not available, try again");
+        }
+    }
+
+}
+
