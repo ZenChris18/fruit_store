e0e06d5f1d59d2de2bc730ccb31375eb8f2029a2
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-06-07 20:33:31 +0800

    added the cart feature

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@ class Program
 {
     static Dictionary<string, int> prices = new Dictionary<string, int>
      {
-            {"apple", 50},
-            {"mango", 70},
-            {"banana", 45}
+        {"apple", 50},
+        {"mango", 70},
+        {"banana", 45},
+        {"berry", 30}
     };
 
+    static Dictionary<string, int> cart = new Dictionary<string, int>
+     {
+
+    };  
 
     static void Main(string[] args)
     {
@@ -30,19 +35,42 @@ class Program
     {
         while (true)
         {
+            Console.WriteLine("Type cart to see your cart");
             Console.Write("What fruit would you like to buy: (type exit to stop program): ");
             String? fruit = Console.ReadLine();
             if (fruit == "exit")
             {
                 break;
             }
-            else if (fruit != null)
+            else if (fruit == "cart")
             {
-                Price(fruit);
+                int price = 0;
+
+                foreach (var item in cart)
+                {
+                    Console.WriteLine(item.Key + " - " + item.Value);
+                }
+                foreach( var item in cart)
+                {
+                    price += prices[item.Key] * item.Value;
+                }
+                Console.WriteLine("Total price: " + price + "$");
+            }
+            else if (fruit != null && prices.ContainsKey(fruit.ToLower()))
+            {
+                if (cart.ContainsKey(fruit.ToLower()))
+                {
+                    cart[fruit.ToLower()] += 1;
+                }
+                else
+                {
+                    cart.Add(fruit.ToLower(), 1);
+                    Console.WriteLine("you added " + fruit + " to your cart");
+                }
             }
             else
             {
-                Console.WriteLine("No fruit entered");
+                Console.WriteLine("Please enter a valid fruit name or type 'cart' to view your cart.");
             }
         }
     }
@@ -61,6 +89,5 @@ class Program
             Console.WriteLine("That fruit is not available");
         }
     }
-
 }
 
